86cb6a6c764d988f097109ea3a39698214021edd
freshprinceofcoders <milanosolanki27@gmail.com>
2020-07-14 19:05:46 +0100

    finished Q1

--- game.py
+++ game.py
@@ -1,1 +1,32 @@
 
+import datetime
+
+
+print(datetime.datetime.now())
+def my_num(n):
+    num = 1000
+    three_times_table = []
+    for n in range(num):
+        if n % 3 == 0 or n % 5 == 0:
+            three_times_table.append(n)
+            print(sum(three_times_table))
+        
+my_num('n')            
+
+print(datetime.datetime.now())
+
+
+'''
+print(datetime.datetime.now())
+
+
+print(datetime.datetime.now())
+sum = 0
+for n in range (1000):
+    if n%3==0 or n%5==0:
+        sum = sum + n
+
+print(sum)
+
+print(datetime.datetime.now())
+'''
